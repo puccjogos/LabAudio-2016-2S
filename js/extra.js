a7a51f566fe81557548d4c6bd2bdbbb945d66f2a
Enric Llagostera <enricllagostera@gmail.com>
2016-06-01 11:09:05 -0300

    Deployed 7eab42a with MkDocs version: 0.15.3

--- js/extra.js
+++ js/extra.js
@@ -1,3 +1,3 @@
-$('a[rel="next"]').html('<i class="fa fa-arrow-left"></i> Anterior');
-$('a[rel="prev"]').html('Próxima <i class="fa fa-arrow-right"></i>');
+$('a[rel="next"]').html('<i class="fa fa-arrow-left"></i>');
+$('a[rel="prev"]').html('<i class="fa fa-arrow-right"></i>');
 $("footer").hide();
